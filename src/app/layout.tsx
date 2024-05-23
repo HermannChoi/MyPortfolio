b437460f953bdac164d260c035c3cb4a0afdfa15
HermannChoi <hiyunseok347@gmail.com>
2024-05-23 15:02:22 +0900

    [docs]모바일 뷰 확대 방지 시도(3)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,12 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         ></meta>
-      </Head>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
